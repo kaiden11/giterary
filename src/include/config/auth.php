5523e8f626952eca9378bd8b377fd8f5494807f5
Josh Rhoades <josh_rhoades@chugachelectric.com>
2015-12-03 15:19:44 -0900

    Removing deployment-specific information that snuck into a past commit, defaulting to password CSV authentication.

--- src/include/config/auth.php
+++ src/include/config/auth.php
@@ -9,29 +9,39 @@ $registered_userlist_calls = array();
 GLOBAL $application_name;
 GLOBAL $instance_name;
 
+## Authentication Modules 
 $pw_authenticator = new PasswordFile( 
     "/var/lib/$application_name/auth/$instance_name/passfile.csv"
 );
 
+/*
 $ad_authenticator = new LDAPAuthenticator( 
-    'ldap://ceav001.chugachelectric.com',
+    'ldap://ldap.yourdomain.com',
     array(
-        'domain'        =>  'CHUGACHELECTRIC',
+        'domain'        =>  'WINDOWSDOMAIN',
         'username_attr' =>  'samaccountname',
-        'base_dn'       =>  'OU=CEA,DC=chugachelectric,DC=com'
+        'base_dn'       =>  'OU=department,DC=yourdomain,DC=com'
     )
 );
+*/
 
+
+## Module Registration for login
+
+/*
 register_login_call( 
     $ad_authenticator,
     "validate_login" 
 );
+*/
 
 register_login_call( 
     $pw_authenticator,
     "validate_login" 
 );
 
+
+## Module Registration for userlist
 
 register_userlist_call( 
     $authenticator,
